6507dc7b5f24b2ab5885c6e49db80ce5f001d849
Yuvashrees150 <88548912+Yuvashrees150@users.noreply.github.com>
2023-06-03 12:40:01 +0530

    overall first commit

---
+++ data.js
@@ -0,0 +1,114 @@
+const data = [
+  {
+    name: "jjawe",
+    author: "Jane Austen",
+    tags: ["Education", "nji"],
+    year: 1990,
+    description:
+      "Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.",
+    image: "book1.jpg",
+  },
+  {
+    name: "2ajawe",
+    author: "2abhji",
+    tags: ["Education", "History"],
+    year: 1990,
+    description:
+      "Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.",
+    image: "book2.jpg",
+  },
+  {
+    name: "3jawe",
+    author: "Jane Austen",
+    tags: ["vgy", "nji"],
+    year: 1990,
+    description:
+      "Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.",
+    image: "book2.jpg",
+  },
+  {
+    name: "4ajawe",
+    author: "Jane Austen",
+    tags: ["-avgy", "Novel"],
+    year: 1990,
+    description:
+      "Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.",
+    image: "book3.jpg",
+  },
+  {
+    name: "5jawe",
+    author: "5bhji",
+    tags: ["Novel", "nji"],
+    year: 1990,
+    description:
+      "Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.",
+    image: "book3.jpg",
+  },
+  {
+    name: "6ajawe",
+    author: "6abhji",
+    tags: ["avgy", "Novel"],
+    year: 1990,
+    description:
+      "Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.",
+    image: "book1.jpg",
+  },
+  {
+    name: "7jawe",
+    author: "7bhji",
+    tags: ["Crime", "nji"],
+    year: 1990,
+    description:
+      "Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.",
+    image: "book1.jpg",
+  },
+  {
+    name: "8ajawe",
+    author: "8abhji",
+    tags: ["RomCom", "anji"],
+    year: 1990,
+    description:
+      "Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.",
+    image: "book4.jpg",
+  },
+  {
+    name: "9jawe",
+    author: "9bhji",
+    tags: ["RomCom", "nji"],
+    year: 1990,
+    description:
+      "Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.",
+    image: "book4.jpg",
+  },
+  {
+    name: "11ajawe",
+    author: "11abhji",
+    tags: ["Crime", "RomCom"],
+    year: 1990,
+    description:
+      "Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.",
+    image: "book2.jpg",
+  },
+  {
+    name: "22j22awe",
+    author: "22bhji",
+    tags: ["Crime", "nji"],
+    year: 1990,
+    description:
+      "Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.",
+    image: "book3.jpg",
+  },
+  {
+    name: "33ajawe",
+    author: "33abhji",
+    tags: ["Crime", "Novel"],
+    year: 1990,
+    description:
+      "Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.",
+    image: "book1.jpg",
+  },
+];
+
+const authors = ["Jane Austen", "Rowling", "Chetan"];
+
+const tags = ["Crime", "RomCom", "Novel", "Education", "History"];
